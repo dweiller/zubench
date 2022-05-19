78aae76752e0701b503ef35ddf094ac44eb7d353
dweiller <4678790+dweiller@users.noreplay.github.com>
2022-05-20 01:09:38 +1000

    refactor: pass const slices to statistical functions

--- src/statistics.zig
+++ src/statistics.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 /// Sample mean
-pub fn mean(samples: []u64) f32 {
+pub fn mean(samples: []const u64) f32 {
     std.debug.assert(samples.len > 0);
     var acc: u128 = 0;
     for (samples) |sample| {
@@ -10,7 +10,7 @@ pub fn mean(samples: []u64) f32 {
     return @intToFloat(f32, acc) / @intToFloat(f32, samples.len);
 }
 
-fn totalSquaredError(samples: []u64, avg: f32) f32 {
+fn totalSquaredError(samples: []const u64, avg: f32) f32 {
     var acc: f32 = 0.0;
     for (samples) |sample| {
         const diff = avg - @intToFloat(f32, sample);
@@ -19,11 +19,11 @@ fn totalSquaredError(samples: []u64, avg: f32) f32 {
     return acc;
 }
 /// sample variance
-pub fn variance(samples: []u64, avg: f32) f32 {
+pub fn variance(samples: []const u64, avg: f32) f32 {
     return totalSquaredError(samples, avg) / @intToFloat(f32, samples.len - 1);
 }
 
 /// sample standard deviation
-pub fn correctedSampleStdDev(samples: []u64, avg: f32) f32 {
+pub fn correctedSampleStdDev(samples: []const u64, avg: f32) f32 {
     return std.math.sqrt(variance(samples, avg));
 }
